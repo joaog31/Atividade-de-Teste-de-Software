599e8ef532f1b28e54e46466484c2ace87704f9e
João Gabriel da Costa Oliveira <163690986+joaog31@users.noreply.github.com>
2026-04-08 17:46:53 -0300

    colocando o  projeto no padrão maven

--- src/test/java/br/com/meujogo/LutaDeDadosTest.java
+++ src/test/java/br/com/meujogo/LutaDeDadosTest.java
@@ -7,7 +7,7 @@ import java.util.Random;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class LutaDeDadosTest {
+public class LutaDeDadosTest {
 
 	private LutaDeDados luta;
 
