3f7cd474be594da2c5e9e138823d128df9ab0ff3
João Gabriel da Costa Oliveira <163690986+joaog31@users.noreply.github.com>
2026-04-08 16:46:47 -0300

    Adicionando um sysout que indica que o jogo começou

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,8 @@ public class Program {
 	public static void main(String[] args) {
 
 		LutaDeDados ldd = new LutaDeDados();
-
+			
+		System.out.println("jogo começou!\n");
 		while (ldd.getJogador1Vida() > 0 && ldd.getJogador2Vida() > 0) {
 			System.out.println(ldd.turno(15, 15));
 			System.out.println(ldd.verificarVencedor());
